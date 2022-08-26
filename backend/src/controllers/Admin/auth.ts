0ae05f6e502a72e1712bbfbeff6db5d5ad179d2c
JamshidbekJS <jamshidbekml@gmail.com>
2022-08-26 11:30:42 +0500

    mistake fixed

--- backend/src/controllers/Admin/auth.ts
+++ backend/src/controllers/Admin/auth.ts
@@ -116,17 +116,4 @@ export const getUserByName = asyncMiddleware(async function(req: Request, res: R
   res.status(200).json({ success: true, user })
 })
 
-// @desc Get all banks
-// @route /auth/banks
-// access Admin
-export const getAllBanks = asyncMiddleware(async (req: Request, res: Response) => {
-  const { rows } = await getData(`SELECT client_code,bank_name,
-                                    case when residency = 1 then 'Локал банк'
-                                    else 'Иностранные банк' end          as residency,
-                                    case when rating_status = 1 then 'Инвестиционный'
-                                    else 'Не инвестиционный' end as rating_status
-                                  FROM bank_info_rating`)
-  res.status(200).json({ success: true, banks: rows })
-})
-
 
