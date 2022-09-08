6a6d1bb49cf670b2230f6d816b61b81d0e1a23bc
Yoriqulov Oybek <hhoybek@gmail.com>
2022-09-08 14:48:10 +0500

    daily indicators fallback updated

--- frontend/src/components/UI/helpers/FakeSuspense.tsx
+++ frontend/src/components/UI/helpers/FakeSuspense.tsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect, PropsWithChildren } from 'react'
+import { LinearProgress } from '@material-ui/core'
+
+function LinearFallback() {
+  return <div style={{ margin: '20px 0' }}>
+    <LinearProgress /></div>
+}
 
 interface FakeSuspenseProps {
     delay?: any
     fallback?: any
 }
 
-const FakeSuspense: React.FC<PropsWithChildren<FakeSuspenseProps>> = ({ children, delay = 500, fallback = <div/> }) => {
+const FakeSuspense: React.FC<PropsWithChildren<FakeSuspenseProps>> = ({
+  children,
+  delay = 500,
+  fallback = <LinearFallback/>
+}) => {
   const [isShown, setIsShown] = useState(false)
 
   useEffect(() => {
