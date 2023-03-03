8b1c9895f30be158bf6a44b513c94c264b1098f4
Yoriqulov Oybek <hhoybek@gmail.com>
2023-03-03 18:23:24 +0500

    capital excel done

--- frontend/src/utils/excelProcessor.ts
+++ frontend/src/utils/excelProcessor.ts
@@ -1,4 +1,4 @@
-import { WorkSheet } from 'sheetjs-style'
+import { ColInfo, WorkSheet } from 'sheetjs-style'
 
 const headerCellStyles = {
 	fill: {
@@ -26,7 +26,45 @@ const simpleCellStyles = {
 export class ExcelProcessor {
 	constructor(private readonly workSheet: WorkSheet, private readonly data?: Record<string, unknown>) {}
 
-	keyIndicatorsBaseProcessor(boldCells: number[]) {
+	setCapStyles = (key: string) => {
+		this.workSheet[key].s = {
+			font: {
+				bold: true,
+				italic: true
+			},
+			alignment: {
+				horizontal: 'right'
+			}
+		}
+	}
+
+	setColsWidth = (colsWidth: ColInfo[]) => {
+		this.workSheet['!cols'] = colsWidth
+	}
+
+	setSimpleCellStyles = (workSheetKey: string) => {
+		this.workSheet[workSheetKey].t = 's'
+		this.workSheet[workSheetKey].v = String(this.workSheet[workSheetKey].v).replace('.', ',').replace(/[()]/g, '')
+		this.workSheet[workSheetKey].s = simpleCellStyles
+	}
+
+	setBoldRows = (workSheetKey: string, boldCells: number[]) => {
+		for (const boldCell of boldCells) {
+			if (new RegExp(String.raw`[A-Z]${boldCell}$`, 'g').test(workSheetKey)) {
+				this.workSheet[workSheetKey].s = {
+					alignment: {
+						horizontal: workSheetKey.startsWith('B') ? 'left' : 'center'
+					},
+					font: {
+						bold: true
+					}
+				}
+			}
+		}
+	}
+
+	keyIndicatorsBaseProcessor = (boldRows: number[]) => {
+		const tableCapKey = 'G1'
 		for (const workSheetKey in this.workSheet) {
 			const headerCells = ['A2', 'C2', 'D2', 'E2', 'F2', 'G2']
 			if (['E2', 'D2', 'C2'].includes(workSheetKey)) {
@@ -43,51 +81,43 @@ export class ExcelProcessor {
 				this.workSheet[workSheetKey].s = headerCellStyles
 				continue
 			}
-			if (!workSheetKey.startsWith('B') && workSheetKey !== '!ref' && workSheetKey !== 'G1') {
-				this.workSheet[workSheetKey].t = 's'
-				this.workSheet[workSheetKey].v = String(this.workSheet[workSheetKey].v).replace('.', ',').replace(/[()]/g, '')
-				this.workSheet[workSheetKey].s = simpleCellStyles
+			if (!workSheetKey.startsWith('B') && workSheetKey !== '!ref' && workSheetKey !== tableCapKey) {
+				this.setSimpleCellStyles(workSheetKey)
 			}
-			for (const boldCell of boldCells) {
-				if (new RegExp(String.raw`[A-Z]${boldCell}$`, 'g').test(workSheetKey)) {
-					this.workSheet[workSheetKey].s = {
-						alignment: {
-							horizontal: workSheetKey.startsWith('B') ? 'left' : 'center'
-						},
-						font: {
-							bold: true
-						}
-					}
-				}
-			}
-			if (workSheetKey === 'G1') {
-				this.workSheet['G1'].s = {
-					font: {
-						bold: true,
-						italic: true
-					},
-					alignment: {
-						horizontal: 'right'
-					}
-				}
-			}
+			this.setBoldRows(workSheetKey, boldRows)
+			this.setCapStyles(tableCapKey)
 		}
-		this.workSheet['!cols'] = [
-			{ wpx: 20 },
-			{ wpx: 280 },
-			{ wpx: 100 },
-			{ wpx: 100 },
-			{ wpx: 100 },
-			{ wpx: 150 },
-			{ wpx: 180 }
-		]
+		this.setColsWidth([{ wpx: 20 }, { wpx: 280 }, { wpx: 100 }, { wpx: 100 }, { wpx: 100 }, { wpx: 150 }, { wpx: 180 }])
 	}
 
-	mainIndicators() {
+	mainIndicators = () => {
 		this.keyIndicatorsBaseProcessor([3, 6, 9, 12, 15, 16, 17, 18, 19, 20, 23, 27, 28, 29, 30, 31, 32, 33, 34])
 	}
 
-	profitAndLost() {
+	profitAndLost = () => {
 		this.keyIndicatorsBaseProcessor([7, 11, 12, 14, 18, 19, 28, 29, 30, 31])
+	}
+
+	capital = () => {
+		const tableCapKey = 'C1'
+		const boldRows = [3, 4, 7, 8, 14, 16, 20, 22, 26, 29, 30, 37, 38, 39]
+		for (const workSheetKey in this.workSheet) {
+			const headerCells = ['A2', 'B2', 'C2']
+			if (headerCells.includes(workSheetKey)) {
+				this.workSheet[workSheetKey].s = headerCellStyles
+				continue
+			}
+			if (!workSheetKey.startsWith('B') && workSheetKey !== '!ref' && workSheetKey !== tableCapKey) {
+				this.workSheet[workSheetKey].t = 's'
+				if (!workSheetKey.startsWith('A')) {
+					this.workSheet[workSheetKey].v = String(this.workSheet[workSheetKey].v).replace('.', ',').replace(/[()]/g, '')
+				}
+				this.workSheet[workSheetKey].s = simpleCellStyles
+			}
+			this.setBoldRows(workSheetKey, boldRows)
+			this.workSheet['B3'].s = { alignment: { horizontal: 'center' }, font: { bold: true } }
+			this.setCapStyles(tableCapKey)
+		}
+		this.setColsWidth([{ wpx: 40 }, { wpx: 910 }, { wpx: 100 }])
 	}
 }
